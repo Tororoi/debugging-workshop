338781974d9a7a86ef62e2de959a0e15f1626dd5
Tororoi <cantwell.tom@gmail.com>
2020-03-17 10:46:06 -0400

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
   const jokeList = document.getElementById('joke-list')
   const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
+  let username = document.getElementById('name-input').value
   let joke;
 
   function fetchJoke(){
@@ -12,16 +12,20 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     })
     .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
+    .then(jokeData => {
+      joke = jokeData.joke
+      newJokeLi.innerHTML = `
+      <span class="username">${username} says:</span> ${joke}
+      `
+      jokeList.appendChild(newJokeLi)
+    })
   }
 
   form.addEventListener('submit', (event) => {
-
-    if(username === "") return;
+    event.preventDefault()
+    username = document.getElementById('name-input').value
+    if (username === "") return;
     fetchJoke()
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says:</span> ${joke}
-    `
-    jokeList.appendChild(newJokeLi)
+
   })
 })
